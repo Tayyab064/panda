c966ee75ec0135d68c80b39f09fb4bb32cc89858
Tayyab064 <muhammad.tayyabmukhtar@yahoo.com>
2016-11-17 16:35:04 +0500

    panda

---
+++ Order online food delivery from Lahore's best restaurants_files/hotjar-63377.js
@@ -0,0 +1,10 @@
+window.hjSiteSettings = window.hjSiteSettings || {"testers_widgets":[],"surveys":[],"record_targeting_rules":[],"recording_capture_keystrokes":true,"polls":[],"site_id":63377,"forms":[],"record":false,"heatmaps":[],"deferred_page_contents":[],"feedback_widgets":[],"r":0.6200396825};
+
+window.hjBootstrap = window.hjBootstrap || function (scriptUrl) {
+    var s = document.createElement('script');
+    s.src = scriptUrl;
+    document.getElementsByTagName('head')[0].appendChild(s);
+    window.hjBootstrap = function() {};
+};
+
+hjBootstrap('https://script.hotjar.com/modules-397099a69f05558cbf66f164cffcaf65.js');
